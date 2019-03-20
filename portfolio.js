e1344f0cc20b7066501225161e604dfb348e07c2
Jaysontian <48577877+Jaysontian@users.noreply.github.com>
2019-03-20 11:38:01 -0400

    Update 1.1
    - Minor bug fixes
    - New "Options" Button
    - "Sign In" Screen
    - "What's New" Screen
    - "Share" Screen

--- portfolio.js
+++ portfolio.js
@@ -4,6 +4,14 @@ function backtocode() {window.location.href="../code.html"}
 function backtohome() {window.location.href="index.html"}
 function music() {window.location.href="music.html"}
 function hosa() {window.location.href="notes/hosa.html"}
+function echopage() {
+	var pswrd1 = prompt("Password:", "")
+
+	if (pswrd1 == '828ny74r') {
+		window.location.href="codepages/echo.html"}
+	else {
+		alert("Password incorrect.")}
+}
 
 function admin() {
 	var pswrd = prompt("Password:", "")
@@ -24,4 +32,24 @@ function loadFrame() {
     document.getElementById("body1").className = 'fader';
 };
 
-window.onload = setTimeout(loadFrame, 4000);
+
+var showmorenum = 1;
+function showmore(){
+	if (showmorenum == 1){
+		$( "#moreitems" ).animate({opacity: 1,top: "+=1", height: "toggle"})
+		showmorenum = showmorenum*-1} 
+	else {
+		$( "#moreitems" ).animate({opacity: 0,top: "-=1", height: "toggle"})
+		showmorenum = showmorenum*-1
+}}
+
+function showsignin(){
+	$( "#id01" ).fadeIn(1000)}
+function showshare(){
+	$( "#share" ).fadeIn(1000)}
+function showwhatsnew(){
+	$( "#whatsnew" ).fadeIn(1000)}
+
+
+
+window.onload = setTimeout(loadFrame, 2000);
