41578ba0a92e4b986f379c980f1551187aca5e86
Jaysontian <48577877+Jaysontian@users.noreply.github.com>
2019-04-13 23:47:43 -0400

    Version 1.0

--- ForestryKids/index.js
+++ ForestryKids/index.js
@@ -1,11 +1,11 @@
 document.getElementById('topbar').onmouseover = function(){
-	document.getElementById("mySidenav").style.height = "250px";
+	document.getElementById("mySidenav").style.height = "300px";
 	document.getElementById("mySidenav").style.opacity = "1";
 
 };
 
 document.getElementById('mySidenav').onmouseover = function(){
-	document.getElementById("mySidenav").style.height = "250px";
+	document.getElementById("mySidenav").style.height = "300px";
 	document.getElementById("mySidenav").style.opacity = "1";
 };
 
@@ -48,3 +48,19 @@ var help_note = document.getElementById("tip-help");
 help.onmouseover = function(){help_note.style.display = "block"};
 help.onmouseleave = function(){help_note.style.display = "none"};
 help.onclick = function(){help_note.style.display = "none";};
+
+
+
+function change() {
+			var sel = document.getElementById('selection');
+
+    		var data1 = document.getElementById('piechart');
+    		var data2 = document.getElementById('data2');
+
+    		if(sel.value=='data2'){
+        	data1.style.display = 'block';
+        	data2.style.display = 'none';}
+	    	else{
+	        	data1.style.display = 'none';
+	        	data2.style.display = 'block';}
+    	}
