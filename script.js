74c7d577760480f5493e106c3767c8beaa2496a3
Jayson Tian <jayson.tian@ucc.on.ca>
2019-08-16 12:31:54 -0400

    update 2.5

--- script.js
+++ script.js
@@ -60,19 +60,24 @@ function display_article(docId){
 	        console.log("Document data:", doc.data());
 	        
 	        let con = document.createElement('div');
-	        let title1 = document.createElement('h3');
+	        let title1 = document.createElement('h1');
 			let date1 = document.createElement('I');
 			let body1 = document.createElement('div');
 			let tag1 = document.createElement('p');
 
 			title1.textContent = doc.data().title;
 			body1.innerHTML = doc.data().body;
-			date1.textContent = doc.data().date;
+			date1.textContent = "On " + doc.data().date + " by " + doc.data().author;
 			tag1.textContent = doc.data().category;
+
+			title1.setAttribute('class','article-title');
+			date1.setAttribute('class','article-date');
+			tag1.setAttribute('class','article-tag');
+			body1.setAttribute('class','article-body');
+
 
 			con.appendChild(title1);
 			con.appendChild(date1);
-			con.appendChild(tag1);
 			con.appendChild(body1);
 
 			$('#article-container').append(con);
@@ -100,15 +105,32 @@ function appendfilter(){
 		docRef.onSnapshot(function(querySnapshot) {
 			articles.textContent = '';
 	    	querySnapshot.docs.forEach(function(doc) {
-	    		if (doc.data().category != undefined){
+	    		if (doc.data().category != undefined && doc.data().category != ""){
 	    			$('#sorting').append($('<option>', {
-				    text: doc.data().category,
-				    value: doc.data().category
-				}));
+					    text: doc.data().category,
+					    value: doc.data().category
+					}));
 	    		}
 		    });
-		});
+
+			    var a = new Array();
+			    $("#sorting").children("option").each(function(x){
+			        test = false;
+			        b = a[x] = $(this).val();
+			        for (i=0;i<a.length-1;i++){
+			            if (b ==a[i]) test =true;
+			        }
+			        if (test) $(this).remove();
+			    })
+			});
+
+
 }
+
+
+
+
+
 appendfilter();
 
 
@@ -151,7 +173,6 @@ function render(doc){
 
 	let title = document.createElement('h3');
 	let date = document.createElement('I');
-	let body = document.createElement('div');
 
 	div.setAttribute('article', doc.id);
 	div.setAttribute('tag', doc.data().category);
@@ -159,14 +180,12 @@ function render(doc){
 	div.setAttribute('onclick', 'display_article(\''+ doc.id+'\')');
 
 	title.textContent = doc.data().title;
-	body.innerHTML = doc.data().body.substring(0, 200) + "... ";
 	date.textContent = doc.data().date;
 
 	flex.appendChild(title);
 	flex.appendChild(date);
 
 	div.appendChild(flex);
-	div.appendChild(body);
 
 	articles.appendChild(div);
 
