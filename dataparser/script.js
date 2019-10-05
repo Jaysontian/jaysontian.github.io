b5ed444c54ea2a737d874773facde7aff68dbed3
Jayson Tian <jayson.tian@ucc.on.ca>
2019-10-04 23:18:48 -0400

    dataparser

---
+++ dataparser/script.js
@@ -0,0 +1,50 @@
+function submitData(event) {
+    const selectedFile = document.getElementById('file').files[0];
+    var reader = new FileReader(); reader.onload = onReaderLoad;
+    reader.readAsText(selectedFile);
+}
+
+dataout = {};
+species = 0;
+
+function onReaderLoad(event){
+    var obj = JSON.parse(event.target.result);
+    var i;
+    for (i = 0; i < obj.length; i++) {
+        if (i != 0){
+            if (obj[i].scientific_name == obj[i-1].scientific_name){
+                dataout[obj[i].scientific_name] ++;
+            }
+            else{
+                dataout[obj[i].scientific_name] = 1;
+                species++;
+            }
+        }
+        else{
+            dataout[obj[i].scientific_name] = 1;
+            species++;
+        }
+    }
+
+    document.getElementById('species').innerHTML = "Species Count: "+species;
+    
+    numer = species*(species - 1);
+    denom = 0;
+
+    var datafinal = JSON.stringify(dataout, null, 4);
+    $('#dataset').text(datafinal);
+
+    p = Object.values(dataout);
+
+    var k;
+    for (k=0;k<p.length;k++){
+        denom += p[k]*(p[k]-1);
+    }
+
+    di = numer/denom;
+
+    document.getElementById('diversity').innerHTML = "Simpson's Diversity Index: "+di;
+}
+
+
+
