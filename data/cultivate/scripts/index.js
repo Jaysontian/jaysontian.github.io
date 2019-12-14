02de995f03f8e819027f799b6643bc9aab8e3037
Jayson Tian <jayson.tian@ucc.on.ca>
2019-12-14 15:29:30 -0500

    cultivate

--- data/cultivate/scripts/index.js
+++ data/cultivate/scripts/index.js
@@ -899,7 +899,7 @@ const offline = {
         if (permission === "granted") {
           alert('cool!')
           var notification = new Notification('Notification title', {
-   icon: 'http://cdn.sstatic.net/stackexchange/img/logos/so/so-icon.png',
+   icon: 'img/cell.png',
    body: 'Hey there! You\'ve been notified!',
   });
         }
